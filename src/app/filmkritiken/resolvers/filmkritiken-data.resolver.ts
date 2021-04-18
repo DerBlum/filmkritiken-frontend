33bdd6f4f2f87af0cc5d0ebf70d75549c43e4cfd
Stefan Blum <stefanblum2@gmx.de>
2021-04-18 22:58:39 +0200

    load data from static json file, add more data

--- src/app/filmkritiken/resolvers/filmkritiken-data.resolver.ts
+++ src/app/filmkritiken/resolvers/filmkritiken-data.resolver.ts
@@ -6,166 +6,29 @@ import {
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Bewertung, Film, FilmbewertungDetails, Filmbewertungen, Filmdetails, ImageData } from '../types/DataTypes';
+import * as data from '../../../assets/data/filmkritiken.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmkritikenDataResolver implements Resolve<Array<Filmbewertungen>> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Array<Filmbewertungen>> {
-    return of([
-      <Filmbewertungen>{
-        filmbewertungDetails: <FilmbewertungDetails>{
-          beitragvon: "Dani",
-          besprochenam: new Date(2021, 4 - 1, 4)
-        },
-        film: <Film>{
-          titel: "Enter The Void",
-          image: <ImageData>{
-            source: "EnterTheVoid.jpg",
-            copyright: "IMDb"
-          },
-          details: <Filmdetails>{
-            altersfreigabe: 18,
-            erscheinungsjahr: new Date(2009, 1),
-            regie: "Gaspar Noé",
-            laenge: 161,
-            originaltitel: "Enter the Void",
-            originalsprache: "Englisch, Japanisch",
-            produktionsland: "Frankreich, Japan, Kanada"
-          }
-        },
-        bewertungen: [
-          {
-            von: "Dani",
-            wertung: 7
-          },
-          {
-            von: "Flo",
-            wertung: 4
-          },
-          {
-            von: "Nico",
-            wertung: 2
-          },
-          {
-            von: "Stefan",
-            wertung: 1
-          },
-          {
-            von: "Tiffy",
-            wertung: 1
-          }
-        ] as Array<Bewertung>
-      },
-      <Filmbewertungen>{
-        filmbewertungDetails: <FilmbewertungDetails>{
-          beitragvon: "Flo",
-          besprochenam: new Date(2021, 3 - 1, 28)
-        },
-        film: <Film>{
-          titel: "Chihiros Reise ins Zauberland",
-          image: <ImageData>{
-            source: "chihiro.jpg",
-            copyright: "dummy copyright"
-          },
-          details: <Filmdetails>{
-            altersfreigabe: 0,
-            erscheinungsjahr: new Date(2001, 1),
-            regie: "Hayao Miyazaki",
-            laenge: 125,
-            originaltitel: "千と千尋の神隠し (Sen to Chihiro no Kamikakushi)",
-            originalsprache: "Japanisch",
-            produktionsland: "Japan"
-          }
-        },
-        bewertungen: [
-          {
-            von: "Dani",
-            wertung: 6
-          },
-          {
-            von: "Flo",
-            wertung: 9
-          },
-          {
-            von: "Nico",
-            wertung: 7
-          },
-          {
-            von: "Stefan",
-            wertung: 9
-          },
-          {
-            von: "Tiffy",
-            wertung: 9
-          }
-        ] as Array<Bewertung>
-      },
-      <Filmbewertungen>{
-        filmbewertungDetails: <FilmbewertungDetails>{
-          beitragvon: "Flo",
-          besprochenam: new Date(2021, 3 - 1, 21)
-        },
-        film: <Film>{
-          titel: "Chihiros Reise ins Zauberland",
-          image: <ImageData>{
-            source: "chihiro.jpg",
-            copyright: "dummy copyright"
-          },
-          details: <Filmdetails>{
-            altersfreigabe: 0,
-            erscheinungsjahr: new Date(2001),
-            regie: "Hayao Miyazaki",
-            laenge: 125,
-            originaltitel: "千と千尋の神隠し (Sen to Chihiro no Kamikakushi)",
-            originalsprache: "Japanisch",
-            produktionsland: "Japan"
-          }
-        },
-        bewertungen: [
-          {
-            von: "Flo",
-            wertung: 9
-          },
-          {
-            von: "Stefan",
-            wertung: 9
-          }
-        ] as Array<Bewertung>
-      },
-      <Filmbewertungen>{
-        filmbewertungDetails: <FilmbewertungDetails>{
-          beitragvon: "Flo",
-          besprochenam: new Date(2021, 3 - 1, 21)
-        },
-        film: <Film>{
-          titel: "Chihiros Reise ins Zauberland",
-          image: <ImageData>{
-            source: "chihiro.jpg",
-            copyright: "dummy copyright"
-          },
-          details: <Filmdetails>{
-            altersfreigabe: 0,
-            erscheinungsjahr: new Date(2001),
-            regie: "Hayao Miyazaki",
-            laenge: 125,
-            originaltitel: "千と千尋の神隠し (Sen to Chihiro no Kamikakushi)",
-            originalsprache: "Japanisch",
-            produktionsland: "Japan"
-          }
-        },
-        bewertungen: [
-          {
-            von: "Flo",
-            wertung: 9
-          },
-          {
-            von: "Stefan",
-            wertung: 9
-          }
-        ] as Array<Bewertung>
-      }
-    ] as Array<Filmbewertungen>);
 
+    var jsonString = JSON.stringify((data as any).default);
+    var parsedJson: Array<Filmbewertungen> = JSON.parse(jsonString, dateParser);
+
+    return of(parsedJson);
   }
+
+}
+
+const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+const dateFormatDetailed = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
+
+function dateParser(key, value) {
+  if (typeof value === "string" && (dateFormat.test(value) || dateFormatDetailed.test(value))) {
+    return new Date(value);
+  }
+
+  return value;
 }
