4dcc89218c7fc90ec7a2cb66d27eef6f1b49ef9e
Stefan Blum <stefanblum2@gmx.de>
2021-04-22 23:00:47 +0200

    Add code for testing

--- src/app/filmkritiken/services/filmkritiken.service.ts
+++ src/app/filmkritiken/services/filmkritiken.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { catchError, map } from 'rxjs/operators';
 import { from, Observable, of } from 'rxjs';
 import { Filmbewertungen } from '../types/DataTypes';
+//import * as data from '../../../assets/Filmkritiken.json'
 
 const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
 const dateFormatDetailed = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
@@ -15,6 +16,13 @@ export class FilmkritikenService {
   constructor() { }
 
   getFilmkritiken(): Observable<Array<Filmbewertungen>> {
+
+    //console.log(data)
+    //let json = data as any;
+    //console.log(json.default)
+    //let jsonString = JSON.stringify(json.default);
+    //console.log(jsonString)
+    //return of(JSON.parse(jsonString, dateParser));
 
     let url = 'https://raw.githubusercontent.com/DerBlum/filmkritiken/master/Filmkritiken.json';
     return from(
