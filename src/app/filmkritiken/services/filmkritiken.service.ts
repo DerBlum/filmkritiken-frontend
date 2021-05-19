63b1a33131a04088fb818e549c1dde77232c2567
Stefan Blum <stefanblum2@gmx.de>
2021-05-19 19:30:53 +0200

    adapt API call

--- src/app/filmkritiken/services/filmkritiken.service.ts
+++ src/app/filmkritiken/services/filmkritiken.service.ts
@@ -12,7 +12,7 @@ export class FilmkritikenFrontendService {
   constructor(private filmkritikenService: FilmkritikenService) { }
 
   getFilmkritiken(): Observable<Filmkritiken[]> {
-    return this.filmkritikenService.apiFilmkritikenGet()
+    return this.filmkritikenService.apiFilmkritikenGet(50)
       .pipe(
         map(response => response),
         catchError(err => of(new Array()))
